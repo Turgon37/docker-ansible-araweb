83f0e97447e84fc837b1eef583d159993da32d01
Pierre GINDRAUD <pgindraud@gmail.com>
2020-06-06 00:48:22 +0200

    Rewrite build and tests scripts

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ set -e
 
 # If empty version, fetch the latest from repository
 if [[ -z "$ARAWEB_VERSION" ]]; then
-  ARAWEB_VERSION=`curl --fail -s https://api.github.com/repos/ansible-community/ara-web/tags | grep --perl-regexp --only-matching '(?<=name": ")[0-9.]+' -m 1`
-  if [[ $? -ne 0 ]]; then
+  if ! ARAWEB_VERSION=$(curl --fail -s https://api.github.com/repos/ansible-community/ara-web/tags | \
+                     grep --perl-regexp --only-matching '(?<=name": ")[0-9.]+' -m 1); then
     echo 'Error during fetch last ara-web version'
     exit 1
   fi
@@ -21,37 +21,33 @@ fi
 echo "-> selected ARAWEB version '${ARAWEB_VERSION}'"
 
 # If empty commit, fetch the current from local git rpo
-if [ -n "${SOURCE_COMMIT}" ]; then
+if [[ -n "${SOURCE_COMMIT}" ]]; then
   VCS_REF="${SOURCE_COMMIT}"
-elif [ -n "${TRAVIS_COMMIT}" ]; then
+elif [[ -n "${TRAVIS_COMMIT}" ]]; then
   VCS_REF="${TRAVIS_COMMIT}"
 else
-  VCS_REF="`git rev-parse --short HEAD`"
+  VCS_REF="$(git rev-parse --short HEAD)"
 fi
 test -n "${VCS_REF}"
 echo "-> current vcs reference '${VCS_REF}'"
 
 # Get the current image static version
-image_version=`cat VERSION`
+image_version=$(cat VERSION)
 echo "-> use image version '${image_version}'"
 
 # Compute variant from dockerfile name
-if ! [ -f ${DOCKERFILE_PATH} ]; then
+if ! [[ -f ${DOCKERFILE_PATH} ]]; then
   echo 'You must select a valid dockerfile with DOCKERFILE_PATH' 1>&2
   exit 1
 fi
-if [[ -n "${IMAGE_VARIANT}" ]]; then
-  image_building_name="${DOCKER_IMAGE}:building_${IMAGE_VARIANT}"
-  echo "-> set image variant '${IMAGE_VARIANT}' for build"
-else
-  image_building_name="${DOCKER_IMAGE}:building"
-fi
+
+image_building_name="${DOCKER_IMAGE}:building"
 echo "-> use image name '${image_building_name}' for build"
 
 ## Build image
 echo "=> building '${image_building_name}' with image version '${image_version}'"
 docker build --build-arg "ARAWEB_VERSION=${ARAWEB_VERSION}" \
-             --label "org.label-schema.build-date=`date -u +'%Y-%m-%dT%H:%M:%SZ'`" \
+             --label "org.label-schema.build-date=$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
              --label 'org.label-schema.name=ansible-araweb' \
              --label 'org.label-schema.description=ARA Front web application' \
              --label 'org.label-schema.url=https://github.com/Turgon37/docker-ansible-araweb' \
--- tests.sh
+++ tests.sh
@@ -7,19 +7,13 @@ DOCKER_IMAGE="${DOCKER_REPO:-ansible-araweb}"
 ## Initialization
 set -e
 
-if [[ -n "${IMAGE_VARIANT}" ]]; then
-  image_building_name="${DOCKER_IMAGE}:building_${IMAGE_VARIANT}"
-  image_tags_prefix="${IMAGE_VARIANT}-"
-  echo "-> set image variant '${IMAGE_VARIANT}' for build"
-else
-  image_building_name="${DOCKER_IMAGE}:building"
-fi
-docker_run_options='--detach'
+image_building_name="${DOCKER_IMAGE}:building"
+#docker_run_options='--detach'
 echo "-> use image name '${image_building_name}' for tests"
 
 
 ## Prepare
-if [[ -z $(which container-structure-test 2>/dev/null) ]]; then
+if [[ -z $(command -v container-structure-test 2>/dev/null) ]]; then
   echo "Retrieving structure-test binary...."
   if [[ -n "${TRAVIS_OS_NAME}" && "$TRAVIS_OS_NAME" != 'linux' ]]; then
     echo "container-structure-test only released for Linux at this time."
@@ -28,17 +22,24 @@ if [[ -z $(which container-structure-test 2>/dev/null) ]]; then
   else
     curl -sS -LO https://storage.googleapis.com/container-structure-test/latest/container-structure-test-linux-amd64 \
     && chmod +x container-structure-test-linux-amd64 \
-    && sudo mv container-structure-test-linux-amd64 /usr/local/bin/container-structure-test
+    && mv container-structure-test-linux-amd64 container-structure-test
   fi
 fi
 
 # Download tools shim.
 if [[ ! -f _tools.sh ]]; then
-  curl -L -o ${PWD}/_tools.sh https://gist.github.com/Turgon37/2ba8685893807e3637ea3879ef9d2062/raw
+  curl -L -o "${PWD}/_tools.sh" https://gist.github.com/Turgon37/2ba8685893807e3637ea3879ef9d2062/raw
 fi
-source ${PWD}/_tools.sh
+# shellcheck disable=SC1090
+source "${PWD}/_tools.sh"
 
 
 ## Test
-container-structure-test \
+# shell scripts tests
+# shellcheck disable=SC2038
+find . -name '*.sh' | xargs shellcheck docker-entrypoint.d/*
+
+
+# Image tests
+./container-structure-test \
     test --image "${image_building_name}" --config ./tests.yml
